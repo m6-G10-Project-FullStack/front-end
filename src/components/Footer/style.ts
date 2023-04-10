a408351b036ea788101f7f6ec0f359fe1b944d4d
Bruno Paulin <bruno.p-@hotmail.com>
2023-04-10 14:10:58 -0300

    feat: add footer component

---
+++ src/components/Footer/style.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 310.34px;
+  padding: 45px 55.5px;
+  background-color: var(--gray-0);
+
+  @media (min-width: 768px) {
+    max-width: 1600px;
+    flex-direction: row;
+  }
+
+  img {
+    color: var(--white-fixed);
+  }
+
+  p {
+    text-align: center;
+    width: 265px;
+    color: var(--white-fixed);
+    font-weight: var(--weight-4);
+    font-size: var(--body-2);
+    line-height: 24px;
+  }
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    width: 53px;
+    background-color: var(--gray-1);
+    color: var(--white-fixed);
+    border-radius: 4px;
+  }
+`;
