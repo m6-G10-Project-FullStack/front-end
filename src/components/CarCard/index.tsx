5ff27bdf8882126f7bb97a8b81bb6af153c1b17c
Daniel Carvalho <103118785+DanielZarkos@users.noreply.github.com>
2023-04-10 15:03:02 -0300

    creating CarCard

---
+++ src/components/CarCard/index.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+const CarCard = ({
+  carImage,
+  carDescription,
+  carKilometers,
+  carYear,
+  carPrice,
+}) => {
+  return (
+    <div className="card">
+      <div className="image-container">
+        <Image src={carImage} width={500} height={300} alt="Car Image" />
+      </div>
+      <div className="description">
+        <p>{carDescription}</p>
+      </div>
+      <div className="car-details">
+        <span>{carKilometers} KM</span>
+        <span>{carYear}</span>
+        <span>R$ {carPrice.toFixed(2)}</span>
+      </div>
+      <div className="button-container">
+        <button className="edit-button">Editar</button>
+        <button className="details-button">Ver detalhes</button>
+      </div>
+    </div>
+  );
+};
+
+export default CarCard;
