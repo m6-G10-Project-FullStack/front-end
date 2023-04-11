e954072de068ccacbaad672c7fa1e9eb134080cd
rogeraguiar0 <aguiarroger02@gmail.com>
2023-04-11 16:59:40 -0300

    feat: add correct button styles and removed extra props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,3 @@
-import { Footer } from "../src/components/Footer";
-import { Button } from "../src/components/Button";
-
 export default function Home() {
-  return (
-    <>
-      <Button
-        bg_color="gray-9"
-        b_color="gray-1"
-        h_b_color="gray-1"
-        t_color="gray-1"
-        h_bg_color="gray-1"
-        h_t_color="gray-9"
-        variant="small"
-        clickFunction={() => console.log("teste")}
-      >
-        Teste
-      </Button>
-      <Footer />
-    </>
-  );
+  return <></>;
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,38 +2,19 @@ import { DefaultContainer } from "./style";
 
 interface iButtonProps {
   children: React.ReactNode;
-  b_color: string;
-  bg_color: string;
-  t_color: string;
-  h_b_color: string;
-  h_bg_color: string;
-  h_t_color: string;
   clickFunction: () => void;
-  variant?: "small" | "default";
+  variant: string;
+  size: "small" | "big";
 }
 
 export const Button = ({
   children,
   clickFunction,
-  bg_color,
-  b_color,
-  h_b_color,
-  t_color,
-  h_bg_color,
-  h_t_color,
   variant,
+  size,
 }: iButtonProps) => {
   return (
-    <DefaultContainer
-      b_color={b_color}
-      bg_color={bg_color}
-      t_color={t_color}
-      h_b_color={h_b_color}
-      h_bg_color={h_bg_color}
-      h_t_color={h_t_color}
-      variant={variant}
-      onClick={clickFunction}
-    >
+    <DefaultContainer variant={variant} size={size} onClick={clickFunction}>
       {children}
     </DefaultContainer>
   );
