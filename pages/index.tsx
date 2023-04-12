9bb5f5e75b928569c809d1ee2414ce91cdcd117d
Bruno Paulin <bruno.p-@hotmail.com>
2023-04-12 14:58:36 -0300

    feat: add comment card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,3 @@
-import { List } from "../src/components/FilterList";
-
 export default function Home() {
-  const listaAnos = ["2021", "2022"];
-  const listaCombustivel = ["flex", "elétrico"];
-  const listaCores = ["branco", "prata"];
-  const listaMarcas = ["audi", "renault"];
-  const listaModelos = ["RSQ5", "sandero"];
-  return (
-    <>
-      <List
-        listaAnos={listaAnos}
-        listaCombustivel={listaCombustivel}
-        listaCores={listaCores}
-        listaMarcas={listaMarcas}
-        listaModelos={listaModelos}
-      />
-    </>
-  );
+  return <></>;
 }
