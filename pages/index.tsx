64b30091e59b07b68aeedf11b12c0fb35be0cc4b
rogeraguiar0 <aguiarroger02@gmail.com>
2023-04-12 18:40:04 -0300

    feat: first version of home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,157 @@
-// import { useState } from "react";
-// import { Modal } from "../src/components/ModalWrapper";
-// import { ModalPhoto } from "../src/components/Modal";
+import Head from "next/head";
+import { useState } from "react";
+
+import Image from "../src/assets/car.png";
+import { Button } from "../src/components/Button";
+import { Footer } from "../src/components/Footer";
+import { Banner, CarSection, Container, Pagination } from "./style";
 
 export default function Home() {
-  // const [openModal, setOpenModal] = useState(false);
-  // const carro =
-  //  "https://th.bing.com/th/id/R.4ac4a04a20cc1f2d4b852985e6daabf4?rik=ED1CdiYVJff%2bxQ&pid=ImgRaw&r=0";
+  const [carList, setCarList] = useState([
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 1,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 2,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 3,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 4,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 5,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 6,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 7,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 8,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 9,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 10,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+    {
+      id: 11,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      author: "Róger Aguiar",
+    },
+  ]);
 
-  // return (
-  //   <>
-  //     <button onClick={() => setOpenModal(true)}>Open</button>
-  //     {openModal && (
-  //       <Modal setOpenModal={setOpenModal}>
-  //         <ModalPhoto carImg={carro} setOpenModal={setOpenModal} />
-  //       </Modal>
-  //     )}
-  //     <FilterList />
-  //   </>
-  // );
+  return (
+    <>
+      <Head>
+        <title>
+          Compre e venda carros no Motors Shop - Anúncios de carros novos e
+          usados
+        </title>
+        <meta
+          name="description"
+          content="Compre e venda carros novos e usados no Motors Shop. Anuncie gratuitamente seus veículos e encontre as melhores ofertas."
+        />
+        <meta
+          name="keywords"
+          content="Motors Shop, carros, anúncios, venda, compra, novos, usados"
+        />
+        <meta name="author" content="Motors Shop" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
 
-  return <></>;
+      {/* <Header /> */}
+
+      <main>
+        <Banner image={Image}>
+          <div>
+            <h1>Motors Shop</h1>
+            <p>A melhor plataforma de anúncios de carro do país</p>
+          </div>
+        </Banner>
+        <Container>
+          <Button
+            size="big"
+            variant="brand-2"
+            clickFunction={() => console.log("oi")}
+          >
+            Filtros
+          </Button>
+          {/* <FilterList /> */}
+          <CarSection>
+            {carList.length ? (
+              carList.map((car) => (
+                <li key={car.id} className="card">
+                  <p>{car.name}</p>
+                </li>
+              ))
+            ) : (
+              <li className="advice">Não existem anúncios no momento!</li>
+            )}
+          </CarSection>
+        </Container>
+      </main>
+      <Pagination>
+        <p>
+          <span>1</span> de X
+        </p>
+        <button>Seguinte &gt;</button>
+      </Pagination>
+
+      <Footer />
+    </>
+  );
 }
