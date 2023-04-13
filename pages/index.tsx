28bd20a765954b5acddfcffe012a318e56a9cb9d
Róger Aguiar <103693366+rogeraguiar0@users.noreply.github.com>
2023-04-13 17:35:47 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,212 @@
+import Head from "next/head";
+import { useState } from "react";
+
+import Image from "../src/assets/car.png";
+import { Button } from "../src/components/Button";
 import { Footer } from "../src/components/Footer";
 import { Banner, CarSection, Container, Pagination } from "./style";
 
-import React from "react";
-import { Header } from "../src/components/Header";
-import CommentInput from "../src/components/CommentInput";
-import AsideProfile from "../src/components/AsideProfile";
+import { CarCard } from "../src/components/CarCard";
 
 export default function Home() {
-  const user = {
-    initials: "KD",
-    name: "Kleber Dimas",
-    description:
-      "Lorem ipsolom johnson dejaren rauver brailen. Mandrake sadrago marimbo aren, doro makuke lele. Dusgarov marakov malakoi, garoto espertinho.",
-  };
-  const isLoggedIn = true;
+  const [carList, setCarList] = useState([
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+    {
+      id: 0,
+      name: "Fiat uno",
+      img: Image,
+      description: "Card de test",
+      seller: "Róger Aguiar",
+      km: 1,
+      year: 2019,
+      price: 10000,
+    },
+  ]);
 
   return (
     <>
-      <div>
-        <AsideProfile user={user} />
-        {/* <CommentInput user={user} /> */}
-      </div>
+      <Head>
+        <title>
+          Compre e venda carros no Motors Shop - Anúncios de carros novos e
+          usados
+        </title>
+        <meta
+          name="description"
+          content="Compre e venda carros novos e usados no Motors Shop. Anuncie gratuitamente seus veículos e encontre as melhores ofertas."
+        />
+        <meta
+          name="keywords"
+          content="Motors Shop, carros, anúncios, venda, compra, novos, usados"
+        />
+        <meta name="author" content="Motors Shop" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      {/* <Header /> */}
+
+      <main>
+        <Banner image={Image}>
+          <div>
+            <h1>Motors Shop</h1>
+            <p>A melhor plataforma de anúncios de carro do país</p>
+          </div>
+        </Banner>
+        <Container>
+          <Button
+            size="big"
+            variant="brand-2"
+            clickFunction={() => console.log("oi")}
+          >
+            Filtros
+          </Button>
+          {/* <FilterList /> */}
+          <CarSection>
+            {carList.length ? (
+              carList.map((car, i) => (
+                <CarCard
+                  carName={car.name}
+                  carImg={car.img}
+                  carDescription={car.description}
+                  carSeller={car.seller}
+                  carKm={car.km}
+                  carYear={car.year}
+                  carPrice={car.price}
+                  key={i}
+                />
+              ))
+            ) : (
+              <li className="advice">Não existem anúncios no momento!</li>
+            )}
+          </CarSection>
+        </Container>
+      </main>
+      <Pagination>
+        <p>
+          <span>1</span> de X
+        </p>
+        <button>Seguinte &gt;</button>
+      </Pagination>
+
+      <Footer />
     </>
   );
 }
