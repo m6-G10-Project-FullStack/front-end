86822ae6787b9a3e294ecd5642a77c5e8a79ea52
Daniel Carvalho <103118785+DanielZarkos@users.noreply.github.com>
2023-04-13 17:26:09 -0300

    feat: fix CSS AsideProfile description

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,10 @@ import AsideProfile from "../src/components/AsideProfile";
 
 export default function Home() {
   const user = {
-    initials: "LS",
-    name: "Lucas Simão",
+    initials: "KD",
+    name: "Kleber Dimas",
+    description:
+      "Lorem ipsolom johnson dejaren rauver brailen. Mandrake sadrago marimbo aren, doro makuke lele. Dusgarov marakov malakoi, garoto espertinho.",
   };
   const isLoggedIn = true;
 
