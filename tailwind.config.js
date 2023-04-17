a25acc848fc4c129924a01469c8c53ac62195b14
Bruno Paulin <bruno.p-@hotmail.com>
2023-04-16 23:52:14 -0300

    feat: install tailwind and modifie global.css

---
+++ tailwind.config.js
@@ -0,0 +1,58 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/**/*.{ts, tsx}",
+    "./pages/components/**/*.{ts, tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Lexend", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        // Primary colors
+        brand1: "#4529E6",
+        brand2: "#5126EA",
+        brand3: "#B0A6F0",
+        brand4: "#EDEAFD",
+
+        // Grey scale
+        gray0: "#0B0D0D",
+        gray1: "#212529",
+        gray2: "#495057",
+        gray3: "#868E96",
+        gray4: "#ADB5BD",
+        gray5: "#CED4DA",
+        gray6: "#DEE2E6",
+        gray7: "#E9ECEF",
+        gray8: "#F1F3F5",
+        gray9: "#F8F9FA",
+        gray10: "#FDFDFD",
+        whitefixed: "#FFFFFF",
+
+        // Feedback colors
+        alert1: "#CD2B31",
+        alert2: "#FDD8D8",
+        alert3: "#FFE5E5",
+        success1: "#18794E",
+        success2: "#CCEBD7",
+        success3: "#DDF3E4",
+
+        // Random profile colors
+        random1: "#E34D8C",
+        random2: "#C04277",
+        random3: "#7D2A4D",
+        random4: "#7000FF",
+        random5: "#6200E3",
+        random6: "#36007D",
+        random7: "#349974",
+        random8: "#2A7D5F",
+        random9: "#153D2E",
+        random10: "#6100FF",
+        random11: "#5700E3",
+        random12: "#30007D",
+      },
+    },
+    plugins: [],
+  },
+};
